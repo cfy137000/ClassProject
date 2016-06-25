82c168a6cc5eeb7e63823b512bbce3e85006ae2d
陈丰尧 <734481463@qq.com>
2016-06-25 14:12:17 +0800

    添加悬浮头的RecyclerView，大体效果已经能和蝉游记的详情页面保持一致了

--- 项目A/ChanYouJi/app/src/main/java/com/lanou/chenfengyao/chanyouji/another/ScrollingActivity.java
+++ 项目A/ChanYouJi/app/src/main/java/com/lanou/chenfengyao/chanyouji/another/ScrollingActivity.java
@@ -1,5 +1,6 @@
 package com.lanou.chenfengyao.chanyouji.another;
 
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Rect;
 import android.os.Bundle;
@@ -9,6 +10,8 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
@@ -16,47 +19,54 @@ import android.widget.ImageView;
 
 import com.lanou.chenfengyao.chanyouji.FastBlur;
 import com.lanou.chenfengyao.chanyouji.R;
+import com.timehop.stickyheadersrecyclerview.StickyRecyclerHeadersDecoration;
 
 public class ScrollingActivity extends AppCompatActivity {
-    private NestedScrollView nestedScrollView;
+   // private NestedScrollView nestedScrollView;
     private AppBarLayout appBarLayout;
     private Toolbar toolbar;
     private CollapsingToolbarLayout collapsingToolbarLayout;
-    private ImageView bottomIv,topIv;
+    private ImageView bottomIv, topIv;
     int max;
+
+    private RecyclerView recyclerView;
+    private RvAdapter rvAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scrolling);
-        nestedScrollView = (NestedScrollView) findViewById(R.id.main_scroll);
+       // nestedScrollView = (NestedScrollView) findViewById(R.id.main_scroll);
         collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         bottomIv = (ImageView) findViewById(R.id.bottom_iv);
         topIv = (ImageView) findViewById(R.id.top_iv);
         //处理成毛玻璃的图片
-        topIv.setImageBitmap(FastBlur.doBlur(BitmapFactory.decodeResource(getResources(),R.mipmap.test,new BitmapFactory.Options()),15));
+        topIv.setImageBitmap(FastBlur.doBlur(BitmapFactory.decodeResource(getResources(), R.mipmap.test, new BitmapFactory.Options()), 15));
 
 
-        //滑动的最大距离是 AppBarLayout - toolBar - 状态栏高度
-topIv.setAlpha(0f);
+        topIv.setAlpha(0f);
         appBarLayout = (AppBarLayout) findViewById(R.id.app_bar);
         appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
             @Override
             public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                //滑动的最大距离是 AppBarLayout - toolBar - 状态栏高度
                 //在滑动的时候,动态处理透明度
                 max = appBarLayout.getHeight() - toolbar.getHeight() - getStatusBarHeight();
-                Log.d("ScrollingActivity", "max:" + max);
-                Log.d("ScrollingActivity", "-verticalOffset:" + -verticalOffset);
-                Log.d("ScrollingActivity", "-verticalOffset/(float)max:" + ((-verticalOffset) / (float) max));
-                topIv.setAlpha((-verticalOffset)/(float)max);
+                topIv.setAlpha((-verticalOffset) / (float) max);
+
             }
         });
 
-        //滑动的最大距离
-        max = appBarLayout.getHeight() - toolbar.getHeight() - getStatusBarHeight();
-
+        recyclerView = (RecyclerView) findViewById(R.id.main_rv);
+        rvAdapter = new RvAdapter(this);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(rvAdapter);
+        recyclerView.addItemDecoration(new StickyRecyclerHeadersDecoration(rvAdapter));
     }
 
+
+    //获得状态栏的高度
     private int getStatusBarHeight() {
         Rect rect = new Rect();
         getWindow().getDecorView().getWindowVisibleDisplayFrame(rect);
