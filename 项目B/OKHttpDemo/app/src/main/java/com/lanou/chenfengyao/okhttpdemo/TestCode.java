c4c2840b645ec0bb3ddb2e066441f7f857efb324
陈丰尧 <734481463@qq.com>
2016-07-14 20:49:47 +0800

    OKHTTP

--- 项目B/OKHttpDemo/app/src/main/java/com/lanou/chenfengyao/okhttpdemo/TestCode.java
+++ 项目B/OKHttpDemo/app/src/main/java/com/lanou/chenfengyao/okhttpdemo/TestCode.java
@@ -1,55 +1,55 @@
-package com.lanou.chenfengyao.okhttpdemo;
-
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
-/**
- * Created by ChenFengYao on 16/1/13.
- */
-public class TestCode {
-    public static final String httpUrl = "http://apis.baidu.com/apistore/idservice/id";
-    public static final String httpArg = "id=420984198704207896";
-    String jsonResult = request(httpUrl, httpArg);
-
-
-    /**
-     * @param urlAll
-     *            :请求接口
-     * @param httpArg
-     *            :参数
-     * @return 返回结果
-     */
-    public static String request(String httpUrl, String httpArg) {
-        BufferedReader reader = null;
-        String result = null;
-        StringBuffer sbf = new StringBuffer();
-        httpUrl = httpUrl + "?" + httpArg;
-
-        try {
-            URL url = new URL(httpUrl);
-            HttpURLConnection connection = (HttpURLConnection) url
-                    .openConnection();
-            connection.setRequestMethod("GET");
-            // 填入apikey到HTTP header
-            connection.setRequestProperty("apikey",  "05ac67fa1b96510639766fee04b02c1f");
-            connection.connect();
-            InputStream is = connection.getInputStream();
-            reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
-            String strRead = null;
-            while ((strRead = reader.readLine()) != null) {
-                sbf.append(strRead);
-                sbf.append("\r\n");
-            }
-            reader.close();
-            result = sbf.toString();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return result;
-    }
-
-
-}
+//package com.lanou.chenfengyao.okhttpdemo;
+//
+//import java.io.BufferedReader;
+//import java.io.InputStream;
+//import java.io.InputStreamReader;
+//import java.net.HttpURLConnection;
+//import java.net.URL;
+//
+///**
+// * Created by ChenFengYao on 16/1/13.
+// */
+//public class TestCode {
+//    public static final String httpUrl = "http://apis.baidu.com/apistore/idservice/id";
+//    public static final String httpArg = "id=420984198704207896";
+//    String jsonResult = request(httpUrl, httpArg);
+//
+//
+//    /**
+//
+//     *            :请求接口
+//     * @param httpArg
+//     *            :参数
+//     * @return 返回结果
+//     */
+//    public static String request(String httpUrl, String httpArg) {
+//        BufferedReader reader = null;
+//        String result = null;
+//        StringBuffer sbf = new StringBuffer();
+//        httpUrl = httpUrl + "?" + httpArg;
+//
+//        try {
+//            URL url = new URL(httpUrl);
+//            HttpURLConnection connection = (HttpURLConnection) url
+//                    .openConnection();
+//            connection.setRequestMethod("GET");
+//            // 填入apikey到HTTP header
+//            connection.setRequestProperty("apikey",  "05ac67fa1b96510639766fee04b02c1f");
+//            connection.connect();
+//            InputStream is = connection.getInputStream();
+//            reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+//            String strRead = null;
+//            while ((strRead = reader.readLine()) != null) {
+//                sbf.append(strRead);
+//                sbf.append("\r\n");
+//            }
+//            reader.close();
+//            result = sbf.toString();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        return result;
+//    }
+//
+//
+//}
