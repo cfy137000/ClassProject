dd22d89340c56aff456428027dd9329aa45e2b0b
陈丰尧 <734481463@qq.com>
2016-07-23 19:22:19 +0800

    Server

---
+++ 项目B/AndroidServer/app/src/main/java/com/lanou/chenfengyao/androidserver/SimpleHttpServer.java
@@ -0,0 +1,13 @@
+package com.lanou.chenfengyao.androidserver;
+
+/**
+ * Created by ChenFengYao on 16/7/23.
+ */
+public class SimpleHttpServer {
+    /**
+     * 启动server(异步)
+     */
+    public void startAsync(){
+
+    }
+}
