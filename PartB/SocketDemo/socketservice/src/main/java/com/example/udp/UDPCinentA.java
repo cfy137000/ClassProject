8d065c9da046c9da57a2d3f0f902c7a79025e039
陈丰尧 <734481463@qq.com>
2016-07-27 14:59:42 +0800

    no message

--- PartB/SocketDemo/socketservice/src/main/java/com/example/udp/UDPCinentA.java
+++ PartB/SocketDemo/socketservice/src/main/java/com/example/udp/UDPCinentA.java
@@ -16,20 +16,25 @@ public class UDPCinentA {
         try {
             //用来接收数据 和发送数据的工具
             //定义接收数据的端口
-            DatagramSocket datagramSocket = new DatagramSocket(port);
+            DatagramSocket datagramSocket =
+                    new DatagramSocket(port);
             //每个数据包的大小
             byte[] buffer = new byte[1024];
             //用来接收的Packet
-            DatagramPacket receiver = new DatagramPacket(buffer,1024);
+            DatagramPacket receiver =
+                    new DatagramPacket(buffer,1024);
             System.out.println("客户端A准备接收数据");
             while (true){
                 //一直等待接收数据
                 datagramSocket.receive(receiver);
-                String data = new String(receiver.getData(),receiver.getOffset(),receiver.getLength());
+                String data = new String(receiver.getData(),
+                        receiver.getOffset(),
+                        receiver.getLength());
                 System.out.println("客户端A接收到了数据"+data);
                 //模拟回复数据
                 String recData = "你说的:"+data+" 我知道了";
-                DatagramPacket dpSend = new DatagramPacket(recData.getBytes(), recData.getBytes().length
+                DatagramPacket dpSend = new DatagramPacket(recData.getBytes(),
+                        recData.getBytes().length
                         , receiver.getAddress(), receiver.getPort());
                 datagramSocket.send(dpSend);
             }
